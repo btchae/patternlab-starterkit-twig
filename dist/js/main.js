97b131cdcc738ed2b87d10305fb2cbffddac5ed0
adam <afenton@getmoxied.net>
2016-08-16 17:24:36 -0500

    Update atoms folder names and add imports

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,7 +3,11 @@ require.context( 'atoms/global/base', true, /^\.\/.*\.(js|scss)/ );
 
 // Then all atoms, molecules, etc
 // (for this test we're just including all globals, which will require all except base which is required above)
-require.context( 'atoms/global', true, /^\.\/.*\.(js|scss)/ );
+require.context( 'atoms', true, /^\.\/.*\.(js|scss)/ );
+//
+// // Then just for this test we add hamburger as it has a JS script.
+// require.context( 'atoms/hamburger', true, /^\.\/.*\.(js|scss)/ );
 
-// Then just for this test we add hamburger as it has a JS script.
-require.context( 'atoms/hamburger', true, /^\.\/.*\.(js|scss)/ );
+// require( 'molecules/navigation' );
+
+// require( 'atoms/hamburger' )();
