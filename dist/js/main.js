3f0e543ad892a5eb9a8e2208ca177fc7351839d2
adam <afenton@getmoxied.net>
2016-08-16 17:30:36 -0500

    Update navigation

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,6 +4,7 @@ require.context( 'atoms/global/base', true, /^\.\/.*\.(js|scss)/ );
 // Then all atoms, molecules, etc
 // (for this test we're just including all globals, which will require all except base which is required above)
 require.context( 'atoms', true, /^\.\/.*\.(js|scss)/ );
+require.context( 'molecules/navigation', true, /^\.\/.*\.(js|scss)/ );
 //
 // // Then just for this test we add hamburger as it has a JS script.
 // require.context( 'atoms/hamburger', true, /^\.\/.*\.(js|scss)/ );
