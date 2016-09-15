e8b7d10faecefc04929d9c471b55312edfebaf6d
adam <afenton@getmoxied.net>
2016-09-15 11:02:50 +0100

    #19 Sepoarate dev and build scripts

---
+++ dist/webpack.config.dev.js
@@ -0,0 +1,32 @@
+var ExtractTextPlugin = require( 'extract-text-webpack-plugin' );
+var path = require( 'path' );
+
+module.exports = {
+  entry: './js/main.js',
+  output: {
+    path: __dirname,
+    filename: 'js/bundle.js'
+  },
+  resolve: {
+    extensions: [ '', '.js', 'css', 'scss' ],
+    modulesDirectories: [
+      path.join( __dirname ),
+      path.join( __dirname + '/_patterns' ),
+      path.join( __dirname + '/_patterns/atoms/importable' )
+    ]
+  },
+  cache: true,
+  debug: true,
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract( 'style', 'css!sass' )
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin( 'css/style.css' )
+  ]
+};
